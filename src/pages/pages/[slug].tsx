e092f9d1487ab32b1485759a59dd9bab7e1184ec
cy <hello@cy.my>
2021-11-23 20:09:02 +0800

    chore(app): linting

--- src/pages/pages/[slug].tsx
+++ src/pages/pages/[slug].tsx
@@ -1,13 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import Link from 'next/link';
 
-type Data = {
-  name: string
+interface PageDetailProps {
+  slug: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { slug } = context.query
+
+  return {
+    props: { slug },
+  }
 }
+
+const PageDetail: NextPage<PageDetailProps> = ({ slug }) => {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Id: {' '}
+          <Link href="/" passHref>
+            <a className="text-blue-600" >
+              {slug}
+            </a>
+          </Link>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default PageDetail
