14a720ef732caa27d86478a5b9b835a7429fc4c0
cylim <cylim226@gmail.com>
2021-11-25 15:51:19 +0800

    add(template): template create page

--- src/pages/templates/[templateId]/create.tsx
+++ src/pages/templates/[templateId]/create.tsx
@@ -1,11 +1,14 @@
-import type { GetServerSideProps, GetStaticPropsContext, NextPage } from 'next'
-import Link from 'next/link';
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head';
 import { useTranslations } from 'use-intl';
 import Footer from '../../../components/Footer';
 import Header from '../../../components/Header';
 import SectionHeader from '../../../components/SectionHeader';
 import Constants from '../../../utils/constants';
+import TextInput from '../../../components/Input';
+import { Button, PlainButton } from '../../../components/Button';
+import { useRouter } from 'next/dist/client/router';
+import { MouseEvent } from 'react';
 interface TemplateCreateProps {
   templateId: string
 }
@@ -15,44 +18,55 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   // const res = await fetch('https://api.github.com/repos/vercel/next.js')
   // const errorCode = res.ok ? false : res.statusCode
   // const json = await res.json()
+  if(+(templateId || 99) > 5 || +(templateId || 99) < 1) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
 
   return {
     props: { templateId },
   }
 }
 
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
-  const localeData = await import(`../utils/locales/${locale}.json`)
-  return {
-    props: {
-      messages: JSON.parse(JSON.stringify(localeData))
-    }
-  };
-}
-
-const TemplateCreate: NextPage<TemplateCreateProps> = () => {
+const TemplateCreate: NextPage<TemplateCreateProps> = ({templateId}) => {
   const t = useTranslations('TemplatesCreate');
+  const router = useRouter()
+
+  const handleBack = (e: MouseEvent) => {
+    e.preventDefault()
+    router.back()
+  }
+  const handleCreate = (e: MouseEvent) => {
+    e.preventDefault()
+    // TODO:- Contract Action: Create page
+    router.push('/settings/mint')
+  }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>{t('title_1')} - {Constants.appName}</title>
       </Head>
       <Header />
       <main className="flex flex-col w-full flex-1 px-20 ">
-        <SectionHeader title={t('title_1')} subtitle={t('title_2')}/>
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-          <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <h1 className="text-6xl font-bold">
-              Welcome to{' '}
-              <Link href="/" passHref>
-                <a className="text-blue-600" >
-                  LMA Art!
-                </a>
-              </Link>
-            </h1>
-          </main>
-        </div>
+        <SectionHeader title={t('title_1')} />
+        <form>
+          <div className="grid gap-x-8 gap-y-16 grid-rows-none pt-20 grid-flow-col md:grid-rows-3">
+            <TextInput label={t('Brand Artist Name')} type="text" placeholder={t("Enter Name")} required/>
+            <TextInput label={t('Description')} type="text" placeholder={t("What is this marketplace about?")} classStyle="row-span-2" required multiline/>
+            <TextInput label={t('Custom URL Link')} type="text" placeholder={t("Your favorite slug")} required/>
+            <TextInput label={t('Tags')} type="text" placeholder={t("What is the marketplace related to?")} required/>
+            <TextInput label={t('Marketplace availability')} type="text" placeholder="" required/>
+          </div>
+          <div className="flex flex-row content-center justify-end mt-10 gap-6">
+            <PlainButton title={t('Cancel')} handlePress={handleBack} style="hover:text-red-400" />
+            <Button title={t('Create and Proceed to the Payment')} handlePress={handleCreate}/>
+          </div>
+        </form>
 
       </main>
       <Footer />
